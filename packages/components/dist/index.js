dcd9ee0fa61f1f8d9fd8240e0e0b7ba77f2b3e36
Maria-cris <maria.delacruz@lottuseducation.com>
2023-04-27 18:17:58 -0600

    dependencies

--- packages/components/dist/index.js
+++ packages/components/dist/index.js
@@ -3,6 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "Banner", {
+  enumerable: true,
+  get: function get() {
+    return _Banner.default;
+  }
+});
 Object.defineProperty(exports, "Button", {
   enumerable: true,
   get: function get() {
@@ -10,4 +16,5 @@ Object.defineProperty(exports, "Button", {
   }
 });
 var _Button = _interopRequireDefault(require("./atoms/Button/Button.js"));
+var _Banner = _interopRequireDefault(require("./organisms/Banner/Banner.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
